e5981586c448632d920c02a0775f8e94453550d2
GabrielTavaresX <gabrielpiche13@gmail.com>
2023-08-15 17:17:45 -0300

    Mensagem

---
+++ src/components/Footer/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+import {colors} from "../..styles/GlobalStyle"
+
+ export const Footer = styled.footer`
+ ackground-color: rgb(199, 198, 198);
+ height: 3rem;
+ border-radius: 100px;
+ display: flex;
+ font-family:'khand', sans-serif;
+ font-weight:900;
+  img{
+    eight: 100%;
+    padding: 0.3rem;
+    align-items: center;
+  }
+  `
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,12 @@ import * as S from "./styles.ts"
  export function Header(){
    return(
     <S.Header>
+       <header>
+       <div id="divBusca">
+            <input type="text" id="txtBusca" placeholder="Buscar..." />
+
+        </div>
+       </header>
         <nav>
             
         </nav>
